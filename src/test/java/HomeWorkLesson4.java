ea4d8e51d71ac35574ce23b80aa7b14d133cfdff
Alexander Petyukov <sandrounknown1991@gmail.com>
2024-09-13 12:03:00 +0300

    code fix

--- src/test/java/HomeWorkLesson4.java
+++ src/test/java/HomeWorkLesson4.java
@@ -1,8 +1,6 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.CollectionCondition.itemWithText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -10,22 +8,34 @@ public class HomeWorkLesson4 {
 
     @BeforeAll
     static void beforeAll() {
+        Configuration.baseUrl = "https://github.com";
         Configuration.browserSize = "800x600";
         Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
-    void checkJUnit5CodeOnPage() {
-        open("https://github.com/selenide/selenide");
+    void checkJUnit5CodeOnPageTest() {
+        open("/selenide/selenide");
 
         $("a#wiki-tab").click();
 
          $("input#wiki-pages-filter").setValue("SoftAssertions");
         $$("ul.filterable-active a").findBy(text("SoftAssertions")).click();
 
-        // Какая из проверок правильней?
-        $("div.markdown-body").shouldHave(text("3. Using JUnit5 extend test class:"));                          //нахожу "общий" div и в нем ищу по тексту
-        $$("h4.heading-element").shouldHave(itemWithText("3. Using JUnit5 extend test class:"));    //нахожу коллекцию h4 с классом heading-element и ищу в ней элемент по тексту
-        $$(".heading-element").shouldHave(itemWithText("3. Using JUnit5 extend test class:"));      //нахожу коллекцию элементов с классом heading-element (без жесткого указания искать среди h4) и ищу в ней элемент по тексту
+        // Такой проверки будет достаточно? Вроде как "@ExtendWith" кроме как в JUnit5 нигде более не применяется.
+        $(".markdown-body").shouldHave(text("@ExtendWith"));
+        // Или нужна проверка на полный текст кода? А что если его хоть чуть-чуть поменяют? - Тест уже не пройдет, но это всё-равно будет пример на JUnit5
+        $(".markdown-body").shouldHave(text("""
+                        @ExtendWith({SoftAssertsExtension.class})
+                        class Tests {
+                          @Test
+                          void test() {
+                            Configuration.assertionMode = SOFT;
+                            open("page.html");
+                        
+                            $("#first").should(visible).click();
+                            $("#second").should(visible).click();
+                          }
+                        }"""));
     }
 }
